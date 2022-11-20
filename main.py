3c663c5968b3b7129cc7aae4e2564eabedddbaf1
ReyEduardo836 <reyeduardohalsallquintero8@gmail.com>
2022-11-20 14:59:26 -0600

    Validando Path Parameters

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 #FastAPI
 from fastapi import FastAPI
 from fastapi import Body
-from fastapi import Query
+from fastapi import Query, Path
 
 
 
@@ -31,9 +31,35 @@ def create_person(person:Person = Body(...)):#Los tres puntos "..." significan q
     return person
 
 #Validaciones : Query Parameters
+#127.0.0.1:8000/person/detail?name=John&age=12
 @app.get("/person/detail")
 def show_person(
-    name:Optional[str] = Query(default=None, min_length=1, max_length=20), #opcional
-    age:int = Query(..., ge=5, le=150) #Los tres puntos "..." significan que es obligatorio
+    name:Optional[str] = Query(
+        default=None,
+        min_length=1,
+        max_length=20,
+        title="Person Name",
+        description="This is the person name. It's between 1 and 50 characters"
+        ), #opcional
+    age:int = Query(
+        ...,
+        ge=5,
+        le=150,
+        title="Person Age",
+        description="This is the person age. It's required"
+        ) #Los tres puntos "..." significan que es obligatorio
 ):
     return {name:age}
+
+#Validaciones : Path Parameters
+#127.0.0.1:8000/person/detail/25
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id:int = Path(
+        ...,
+        gt=0,
+        title="Person Id",
+        description="This is the person Id. It's required"
+        )
+): 
+    return {person_id: "it exists!"}
