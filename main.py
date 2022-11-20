ead42da8de6b35d43fe885ab192f7417dc9de2b6
ReyEduardo836 <reyeduardohalsallquintero8@gmail.com>
2022-11-20 14:22:35 -0600

    Validando Query Parameters

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 #FastAPI
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Query
 
 
 
@@ -26,5 +27,13 @@ def home(): #Path Operation Function
 
 #Request and Response Body
 @app.post("/person/new") #Request Body
-def create_person(person:Person = Body(...)):
+def create_person(person:Person = Body(...)):#Los tres puntos "..." significan que es obligatorio
     return person
+
+#Validaciones : Query Parameters
+@app.get("/person/detail")
+def show_person(
+    name:Optional[str] = Query(default=None, min_length=1, max_length=20), #opcional
+    age:int = Query(..., ge=5, le=150) #Los tres puntos "..." significan que es obligatorio
+):
+    return {name:age}
